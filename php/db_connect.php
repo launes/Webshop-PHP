6406cf0704fbf03e12e8656caceb1ebd64d73d49
launes <217ers@gmail.com>
2025-06-03 11:52:02 +0200

    Support for render.com
    added Support for render.com

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,27 +1,24 @@
 <?php
 // Funktion zum Herstellen einer Datenbankverbindung
 function erstelleDatenbankverbindung() {
-    // Hostname des Datenbankservers (bei XAMPP meistens "localhost")
-    $servername = "localhost";
-    // Benutzername für die Datenbank (bei XAMPP meistens "root")
-    $username = "root";
-    // Passwort für die Datenbank (bei XAMPP meistens leer)
-    $password = "";
-    // Name der Datenbank, die verwendet werden soll
-    $dbname = "amazon_clone";
+    // Umgebungsvariablen laden
+    $servername = getenv('DB_HOST') ?: 'localhost';
+    $username = getenv('DB_USER') ?: 'root';
+    $password = getenv('DB_PASS') ?: '';
+    $dbname = getenv('DB_NAME') ?: 'amazon_clone';
 
     // Erstellen eines neuen MySQLi-Objekts für die Verbindung
     $verbindung = new mysqli($servername, $username, $password, $dbname);
 
     // Prüfen, ob die Verbindung geklappt hat
-    if ($verbindung->connect_error) { // Wenn ein Verbindungsfehler aufgetreten ist
-        die("Verbindung fehlgeschlagen: " . $verbindung->connect_error); // Skript beenden und Fehlermeldung anzeigen
+    if ($verbindung->connect_error) {
+        error_log("Datenbankverbindung fehlgeschlagen: " . $verbindung->connect_error);
+        die("Datenbankverbindung konnte nicht hergestellt werden. Bitte versuchen Sie es später erneut.");
     }
 
-    // Zeichensatz der Verbindung auf UTF-8 setzen (wichtig für Umlaute)
+    // Zeichensatz der Verbindung auf UTF-8 setzen
     $verbindung->set_charset("utf8");
 
-    // Die hergestellte Verbindung zurückgeben
     return $verbindung;
 }
 ?>
