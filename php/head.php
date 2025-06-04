be4373a19d488927521a4b03cb196417318d26f3
launes <114355959+launes@users.noreply.github.com>
2025-06-04 14:58:36 +0200

    Add files via upload

--- php/head.php
+++ php/head.php
@@ -2,5 +2,6 @@
 echo '<head>';
 echo '    <meta charset="UTF-8">';
 echo '    <meta name="viewport" content="width=device-width, initial-scale=1.0">';
+echo '    <link rel="stylesheet" href="../css/base.css">';
 echo '</head>';
 ?>
